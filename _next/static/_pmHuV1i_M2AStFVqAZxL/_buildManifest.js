69d61fd7204fb4bc8ffcd6aff9157d72bebdd8a3
ondabii <ondabii@kakao.com>
2026-05-29 06:10:48 +0900

    nojekyll

--- _next/static/_pmHuV1i_M2AStFVqAZxL/_buildManifest.js
+++ _next/static/_pmHuV1i_M2AStFVqAZxL/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/JangsHub//_next/:path+",
+        "destination": "/JangsHub/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
